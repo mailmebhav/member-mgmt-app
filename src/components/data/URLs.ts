3477c97e4405f0f856ccedb96da5ffd2be536bf0
Manish Patel <100991821+manishpatel91@users.noreply.github.com>
2024-08-24 17:24:53 +0530

    integrated counts api in home

--- src/components/data/URLs.ts
+++ src/components/data/URLs.ts
@@ -5,3 +5,5 @@ export const paymentsAPI = 'http://localhost:3000/api/paymentdetail'
 export const membersAPI = 'http://localhost:3000/api/member'
 
 export const paymentTypeAPI = 'http://localhost:3000/api/paymenttype'
+
+export const countsApi = 'http://localhost:3000/api/count'
